3798432ce723cc7df07ad0dce425756bc641a5af
samita100 <66769151+samita100@users.noreply.github.com>
2021-01-03 22:57:10 +0545

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,3 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello world!'
+
+
+
+@app.route('/proxy')
+def hello():
+  response = requests.get('https://www.proxyscan.io/api/proxy?ping=100')
+  jsons = json.loads(response.text)
+  for i in jsons:
+    ip = i["Ip"]
+    port = i["Port"]
+    t = i["Type"]
+    tf = t[0].lower()
+    final = f'{tf}://{ip}:{port}'
+    print(final)
+  return final
