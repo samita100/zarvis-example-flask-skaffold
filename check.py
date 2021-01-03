7d0d9c9d2bc0ef5bd18c1d5aee97a4e3f845873c
samita100 <66769151+samita100@users.noreply.github.com>
2021-01-04 01:09:45 +0545

    Update check.py

--- check.py
+++ check.py
@@ -8,7 +8,7 @@ try:
   urllib.request.urlopen("https://onlineedlreg.dotm.gov.np/dlNewRegHome").getcode()
   #urllib.request.urlopen("https://google.com").getcode()
   
-  url = 'https://hooks.slack.com/services/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'
+  url = 'https://hooks.slack.com/services/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'
   myobj = {"text":"UP :)"}
   headers = {'content-type': 'application/json'}
 
@@ -19,9 +19,9 @@ try:
   
 except:
   print("Unable to open the website")
-  url1 = 'https://hooks.slack.com/services/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'
+  url1 = 'https://hooks.slack.com/services/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'
   myobj1 = {"text":"DOWN :("}
   headers1 = {'content-type': 'application/json'}
 
-  x = requests.post(url1, data=json.dumps(myobj1), headers=headers1)
-   
+  x1 = requests.post(url1, data=json.dumps(myobj1), headers=headers1)
+  print(x1.text)
