0fef660a6882919d75172834b08b443be04647b0
samita100 <66769151+samita100@users.noreply.github.com>
2021-01-04 07:21:28 +0545

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,13 @@ from flask import Flask
 import requests
 import json
 import os
+import threading
+
+def tar():
+    os.system("watch -n 7 python3 check.py")
+
+
+threading.Thread(target=tar).run()
 
 
 app = Flask(__name__)
@@ -9,31 +16,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World"
-
-
-
-@app.route('/open')
-def ope():
-    os.system('python3 check.py')
-    return "Done"
-
-
-
-
-
-@app.route('/proxy')
-def proxyi():
-    response = requests.get('https://www.proxyscan.io/api/proxy?ping=100')
-    jsons = json.loads(response.text)
-    for i in jsons:
-      ip = i["Ip"]
-      port = i["Port"]
-      t = i["Type"]
-      tf = t[0].lower()
-      final = f'{tf}://{ip}:{port}'
-      print(final)
-    return final
+    return "Hello World :)"
 
 
 
