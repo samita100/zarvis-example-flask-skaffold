843f065ab80bd3a282d5bcd041800d986111d390
samita100 <66769151+samita100@users.noreply.github.com>
2021-01-04 07:20:50 +0545

    Update check.py

--- check.py
+++ check.py
@@ -8,18 +8,18 @@ try:
   urllib.request.urlopen("https://onlineedlreg.dotm.gov.np/dlNewRegHome").getcode()
   #urllib.request.urlopen("https://google.com").getcode()
   
-  url = 'https://hooks.slack.com/services/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'
+  url = 'https://hooks.slack.com/services/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'
   myobj = {"text":"UP :)"}
   headers = {'content-type': 'application/json'}
 
   x = requests.post(url, data=json.dumps(myobj), headers=headers)
-
+  print("Website opened")
   print(x.text)
   
   
 except:
   print("Unable to open the website")
-  url1 = 'https://hooks.slack.com/services/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'
+  url1 = 'https://hooks.slack.com/services/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'
   myobj1 = {"text":"DOWN :("}
   headers1 = {'content-type': 'application/json'}
 
